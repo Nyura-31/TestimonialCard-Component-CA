2ca76e156e3896374caaab7e004429ce486997ae
Sathvika B <serein.reverie.meraki@gmail.com>
2025-03-10 09:34:27 +0530

    Full Stack CA

---
+++ client-testimonials/src/Components/TestimonialCard.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+function = () => {
+    const [CustomerName, ReviewText] 
+
+return  (
+
+    <div className="CustomerName"> 
+    
+    <div className= "ReviewText">
+        </div>
+    </div>
+
+);
+
+}
+
+
+
+
+
+
+
+
+export default TestimonialCard
